c4e6bdd96968db9c64f827d4936e0430f279bb21
Sirisha2704 <kanapakasirisha9@gmail.com>
2024-11-26 10:44:19 +0530

    Add files via upload
    JS

---
+++ acc.js
@@ -0,0 +1,37 @@
+
+function hac(){
+    const pin = 9160
+    let x = +prompt("please enter 4 digit pin ")
+    const res = x=== pin;
+    if(res){
+      alert("Granted");
+      window.location.href= "account.html";
+    }else{
+      alert("Denied");
+    }
+    }
+    
+    function cdit(){
+      const pin = 9160
+      let x = +prompt("please enter 4 digit pin ")
+      const res = x=== pin;
+      if(res){
+        alert("crebit Granted");
+        window.location.href= "credit.html";
+      }else{
+        alert("Denied");
+      }
+      }
+    
+      function dit(){
+        const pin = 9160
+        let x = +prompt("please enter 4 digit pin ")
+        const res = x=== pin;
+        if(res){
+          alert(" debit Granted");
+          window.location.href= "debit.html";
+        }else{
+          alert("Denied");
+        }
+        }
+    
